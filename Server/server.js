9ac13a9a2fd3132761b64dcc159983d096f6614f
AkashGautam-27 <sde.akash27@gmail.com>
2026-06-07 15:16:00 +0530

    separate Folders

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
-const app = require("./src/app");
-const connectDB = require("./src/config/db")
+const app = require("../src/app");
+const connectDB = require("../src/config/db")
 connectDB();
 
 app.listen(3000,()=>{
